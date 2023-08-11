bc993241714df0473725908b60154c8011e07226
dkimb0 <jhdavidk@gmail.com>
2023-08-11 00:48:21 -0700

    add minimal password validation

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
+// const form = document.getElementById('form');
 // const firstName = document.getElementById('firstName');
 // const lastName = document.getElementById('lastName');
 // const email = document.getElementById('email');
 // const password = document.getElementById("password");
 // const confirmPassword = document.getElementById('confirmPassword');
 
-// form.addEventListener('submit', e => {
+// form.addEventListener('submit', (e) => {
 //     e.preventDefault();
 
-//     validateInputs();
+//     checkInputs();
 // });
 
-const inputs = document.querySelectorAll("input");
+// function checkInputs(){
+//     const passwordValue = password.value;
+//     const confirmPasswordValue = confirmPassword.value;
 
-inputs.forEach(input => {
-    input.addEventListener(
-            "invalid",
-            event => {
-                input.classList.add("error");
-            },
-            false
-    );
-});
+//     if (passwordValue === confirmPasswordValue)
+// }
+
+
+
+
+
+// const password1 = document.querySelectorAll("#password");
+// const password2 = document.querySelectorAll("confirmPassword");
+
